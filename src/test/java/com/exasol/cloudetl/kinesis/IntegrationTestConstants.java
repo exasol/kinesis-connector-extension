2cba5f7e82521c83f8eba567d89925c3a5808dcc
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-08-25 11:52:29 +0200

    Use same Exasol version if none is specified

--- src/test/java/com/exasol/cloudetl/kinesis/IntegrationTestConstants.java
+++ src/test/java/com/exasol/cloudetl/kinesis/IntegrationTestConstants.java
@@ -3,7 +3,7 @@ package com.exasol.cloudetl.kinesis;
 public class IntegrationTestConstants {
     public static String JAR_FILE_NAME = "exasol-kinesis-connector-extension-1.1.0.jar";
     public static String TEST_SCHEMA_NAME = "kinesis_schema";
-    public static String DEFAULT_EXASOL_DOCKER_IMAGE = "7.1.22";
+    public static String DEFAULT_EXASOL_DOCKER_IMAGE = "7.1.19";
     public static String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:2.2";
     public static String DOCKER_IP_ADDRESS = "172.17.0.1";
     public static final String SHARD_ID = "shardId-000000000000";
