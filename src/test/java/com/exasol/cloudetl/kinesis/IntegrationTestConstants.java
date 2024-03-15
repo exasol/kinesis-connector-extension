10acbbb3e76016e69c604103e9a0dcb6d00cc09f
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-03-15 10:16:17 +0100

    Update localstack version

--- src/test/java/com/exasol/cloudetl/kinesis/IntegrationTestConstants.java
+++ src/test/java/com/exasol/cloudetl/kinesis/IntegrationTestConstants.java
@@ -4,6 +4,6 @@ public class IntegrationTestConstants {
     public static String JAR_FILE_NAME = "exasol-kinesis-connector-extension-1.1.5.jar";
     public static String TEST_SCHEMA_NAME = "kinesis_schema";
     public static String DEFAULT_EXASOL_DOCKER_IMAGE = "7.1.24";
-    public static String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:3.0";
+    public static String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:3.2";
     public static String DOCKER_IP_ADDRESS = "172.17.0.1";
 }
